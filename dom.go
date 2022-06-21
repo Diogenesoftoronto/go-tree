4c1ec47cfe42b098ac6be72bc4923012dd4c9b16
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-06-21 00:12:53 -0400

    change funcs to methods on dom

--- dom.go
+++ dom.go
@@ -1,6 +1,6 @@
 // GO-DOM a go package that implements the document object model in golang
 // the go-dom is a full implementation of the model that browsers implement minus xml namespaces and legacy depreciated features. 
-package tree
+package dom
 
 import "fmt"
 
@@ -38,30 +38,29 @@ func newNode (name string, parent *Node, children []*Node) *Node {
     return &node
 }
 
+// add error handling for functions at some point
 func hasElements (slice []string) bool {
 	return len(slice) < 1
 }
 
-type tables interface {
-	hasElements() bool
-}
+
 
 // returns a node and an interface on a node
-func bfs (node *Node, find string) *Node {
+func bfs (nodePointer *Node, find string) *Node {
+	node := *nodePointer
 	var found Node
 	visited := make(map[string]bool)
 	queue := []Node{}
-	visited[`%s`, node] = true
+	visited[node.name] = true
 	queue = append(queue, node)
 	for !hasElements() {
-
+		
 	}
 
 	return &found
 }
 
-
-func makeTree (nodePointer *Node) *Node {
+func (nodePointer *Node) makeTree() *Node {
 		node := *nodePointer
 		// traverse the nodeSlice and create a new node for each element
 			// empty node pointer list
@@ -75,6 +74,6 @@ func makeTree (nodePointer *Node) *Node {
 		return nodePointer
 }
 
-func sortTree (nodePointer *Node) *Node {
+func (nodePointer *Node) sortTree() *Node {
 	return nodePointer
 }
